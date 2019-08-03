5ef1a76e53fa3539e7b1ddf45492a88dec0c9a93
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-08-02 22:05:52 -0400

    fixed import issue causing app to crash. Began working on action and project router files.

---
+++ projects/projectRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const Projects = require("../data/helpers/projectModel");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const projects = await Projects.get();
+        res.status(200).json(projects);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error getting the projects" });
+    }
+});
+
+module.exports = router;
