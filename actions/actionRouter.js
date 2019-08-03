56881fe55f1b9a7a0a3d399b0ce3185d72d37633
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-08-02 22:52:28 -0400

    wrote out CRUD requests for actions and projects. all working

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -14,4 +14,42 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.post("/", async (req, res) => {
+    try {
+        const action = await Actions.insert(req.body);
+        res.status(201).json(action);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error adding the action" });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const action = await Actions.update(req.params.id, req.body);
+        if (action) {
+            res.status(200).json(action);
+        } else {
+            res.status(404).json({ message: "The action could not be found" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error updating the action" });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const count = await Actions.remove(req.params.id);
+        if (count > 0) {
+            res.status(200).json({ message: "The action has been removed" });
+        } else {
+            res.status(404).json({ message: "The action could not be found" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error removing the action" });
+    }
+});
+
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -14,4 +14,52 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/:id/actions", async (req, res) => {
+    try {
+        const actions = await Projects.getProjectActions(req.params.id);
+        res.status(200).json(actions);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error getting the actions for this project" });
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const project = await Projects.insert(req.body);
+        res.status(201).json(project);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error adding the project" });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const project = await Projects.update(req.params.id, req.body);
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message: "The project could not be found" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error updating the project" });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const count = await Projects.remove(req.params.id);
+        if (count > 0) {
+            res.status(200).json({ message: "The project has been removed" });
+        } else {
+            res.status(404).json({ message: "The project could not be found" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error removing the project" });
+    }
+});
+
 module.exports = router;
